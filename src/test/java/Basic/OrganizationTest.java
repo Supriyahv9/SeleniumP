f0bf2f62ac37f23deb3caeb7e7a25c065e217921
Supriyahv9 <supriyahv9@gmail.com>
2024-12-26 12:06:06 +0530

    project

---
+++ src/test/java/Basic/OrganizationTest.java
@@ -0,0 +1,24 @@
+package Basic;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+	
+	@Test
+	public void CreateOrganizationTest() {
+		
+		WebDriver driver = new ChromeDriver();
+		String URL=System.getProperty("url");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		driver.get(URL);
+		driver.findElement(By.name("user_name")).sendKeys(USERNAME);
+		driver.findElement(By.name("user_password")).sendKeys(PASSWORD);
+		driver.findElement(By.id("submitButton")).click();
+		
+	}
+
+}
